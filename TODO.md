8c9b2c01b7d468a3deeed95bb7c43a017f72cacc
Nicolas Lach <dev@nlach.fr>
2019-12-14 22:05:56 +0100

    Project tree reworked

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# HomeAccountant TODOs
+
+### Package __db__
+
+### Package __log__
+
+### Package __server.api__
--- homeaccountant/config.py
+++ homeaccountant/config.py
@@ -1,6 +1,62 @@
-# Server options
+import os
+import yaml
+import pathlib
 
-LISTEN_ADDRESS = '127.0.0.1'
-LISTEN_PORT = 8080
 
-# Cache configuration
+class SERVER:
+    HOSTNAME = '127.0.0.1'
+    PORT = 8080
+
+    class REGISTRATION:
+        ALLOW = True
+        REGEX = r'.*'
+        EMAIL_CONFIRMATION = False
+
+    class SENDMAIL:
+        ENABLED = False
+        USE_SSL = True
+        HOSTNAME = None
+        PORT = None
+        USERNAME = None
+        PASSWORD = None
+
+
+class DATABASE:
+    class POSTGRES:
+        HOSTNAME = '127.0.0.1'
+        PORT = 5432
+        USERNAME = None
+        PASSWORD = None
+        DATABASE = None
+
+
+def update_attributes(d, prefix=None):
+    for k, v in d.items():
+        k = k.upper()
+        try:
+            if isinstance(v, dict):
+                if prefix == None:
+                    update_attributes(v, prefix=k)
+                else:
+                    update_attributes(v, prefix='.'.join([prefix, k]))
+            else:
+                if prefix == None:
+                    obj = globals()[k]
+                else:
+                    prefixes = prefix.split('.')
+                    obj = globals()[prefixes[0]]
+                    for i in prefixes[1:]:
+                        obj = getattr(obj, i)
+                setattr(obj, k, v)
+        except Exception as e:
+            pass
+
+
+def load_configuration(path):
+    with open(path, 'r') as f:
+        d = yaml.load(f, Loader=yaml.FullLoader)
+    update_attributes(d)
+
+
+load_configuration(os.path.join(str(pathlib.Path.home()),
+                                '.config/homeaccountant/config.yaml'))
