44a735b5adb5a51d05baadabd516acdfa524103e
Nicolas Lach <dev@nlach.fr>
2019-12-14 22:06:39 +0100

    Adding first user related API operations

--- homeaccountant/server/api/app.py
+++ homeaccountant/server/api/app.py
@@ -2,8 +2,10 @@
 import re
 import time
 import asyncio
+import smtplib
 
 from aiohttp import web
+from concurrent.futures import ThreadPoolExecutor
 
 from homeaccountant import config
 from homeaccountant.server.api import user, transaction
@@ -15,7 +17,7 @@ auth_regex = re.compile(r'.*/user/(login|register)$')
 
 @web.middleware
 async def authentication_middleware(request, handler):
-    cache = request.app['cache']
+    cache = request.app.cache
     authorized = False
     try:
         auth_cookie = request.cookies[Cookie.AUTHENTICATION]
@@ -29,11 +31,33 @@ async def authentication_middleware(request, handler):
     return await handler(request)
 
 
+class ConfirmationTokenSender:
+    def __init__(self):
+        self._executor = ThreadPoolExecutor(max_workers=2)
+        if config.SERVER.SENDMAIL.USE_SSL:
+            self._server = smtplib.SMTP_SSL(config.SERVER.SENDMAIL.HOSTNAME, config.SERVER.SENDMAIL.PORT)
+        else:
+            self._server = smtplib.SMTP(config.SERVER.SENDMAIL.HOSTNAME, config.SERVER.SENDMAIL.PORT)
+
+    def _send_token(self, mail_to, subject, token):
+        self._server.connect(config.SERVER.SENDMAIL.HOSTNAME, config.SERVER.SENDMAIL.PORT)
+        self._server.helo()
+        self._server.login(config.SERVER.SENDMAIL.USERNAME, config.SERVER.SENDMAIL.PASSWORD)
+        msg = '''Subject: {subject}
+        {token}
+        '''.format(subject=subject, token=token)
+        self._server.sendmail(config.SERVER.SENDMAIL.USERNAME, mail_to, msg)
+        self._server.quit()
+
+    async def send_token(self, *args, **kwargs):
+        asyncio.get_event_loop().run_in_executor(self._executor, self._send_token, *args, **kwargs)
+
+
 class WebAPI:
     def __init__(self):
         self.__app = web.Application(middlewares=[authentication_middleware])
-        self.__app['cache'] = cache.Cache.create_cache()
-        self.__app['storage'] = None
+        self.__app.cache = None
+        self.__app.storage = None
         self.__site = None
         self.__app.add_routes(user.user_routes)
         self.__app.add_routes(transaction.transaction_routes)
@@ -43,9 +67,11 @@ class WebAPI:
 
     async def run(self):
         runner = web.AppRunner(self.__app)
+        self.__app.cache = cache.Cache.create_cache()
+        self.__app.storage = await storage.Storage.open_storage()
         await runner.setup()
         self.__site = web.TCPSite(
-            runner, config.LISTEN_ADDRESS, config.LISTEN_PORT)
+            runner, config.SERVER.HOSTNAME, config.SERVER.PORT)
         await self.__site.start()
 
 
--- homeaccountant/server/api/user.py
+++ homeaccountant/server/api/user.py
@@ -1,27 +1,61 @@
+import re
+import logging
+import psycopg2.errors
+
+from uuid import uuid4
+from hashlib import sha256
 from aiohttp import web
+from psycopg2.errors import UniqueViolation
+
+from homeaccountant import config
+from homeaccountant.db.utils import User
 
 user_routes = web.RouteTableDef()
 
+regex_email = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+try:
+    regex_email_allowed = re.compile(config.SERVER.REGISTRATION.REGEX)
+except:
+    regex_email_allowed = re.compile(r'.*')
+
 @user_routes.get('/user')
 async def getUser(request):
-    pass
+    raise NotImplementedError
 
 @user_routes.put('/user')
 async def putUser(request):
-    pass
+    raise NotImplementedError
 
 @user_routes.post('/user/register')
 async def registerUser(request):
-    pass
+    if not config.SERVER.REGISTRATION.ALLOW:
+        raise web.HTTPForbidden
+    try:
+        data = await request.json()
+        email = data['email']
+        if not (regex_email.match(email) and regex_email_allowed.match(email)):
+            raise web.HTTPBadRequest
+        password_salt = uuid4().hex
+        password_hash = sha256('{}{}'.format(data['password'], password_salt).encode('utf8')).hexdigest()
+        userlogin = User(email=email, password_hash=password_hash, password_salt=password_salt, enabled=False)
+        # try:
+        #     userlogin = await request.app.storage.add_user(userlogin) # Send JWT with info to store in mail
+        # except UniqueViolation:
+        #     raise web.HTTPOk
+        # Send mail for user confirmation
+    except web.HTTPError:
+        raise
+    except Exception as e:
+        raise
 
 @user_routes.get('/user/confirm')
 async def confirmUser(request):
-    pass
+    raise NotImplementedError
 
 @user_routes.get('/user/login')
 async def loginUser(request):
-    pass
+    raise NotImplementedError
 
 @user_routes.get('/user/logout')
 async def logoutUser(request):
-    pass
+    raise NotImplementedError
